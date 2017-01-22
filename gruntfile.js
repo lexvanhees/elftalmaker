b3e0bb5255ac2750963f1f7a8fc9b86966442b3c
DESKTOP-LexvHees <lex_van_hees@hotmail.com>
2017-01-22 16:56:57 +0100

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+
+    // Config
+    var config = {
+        tmp: '.tmp',
+        base: '.',
+        name: 'amvjvoetbal',
+        css_dest: './contents/'
+    };
+
+
+    grunt.initConfig({
+        // Project settings
+        config: config,
+
+        // Project configuration
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Compile Sass
+        sass: {
+            options: {
+                sourceMap: true,
+                sourceComments: false
+            },
+            dist: {
+                files: {
+                    '<%= config.css_dest %>style.css': '<%= config.css_dest %>style.scss'
+                }
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.css_dest %>',
+                    src: ['*.css', '!*.min.css'],
+                    dest: '<%= config.css_dest %>css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        // Watch and build
+        watch: {
+            files: ['_layouts/*.html', '*.md', '*.yml'],
+            // SASS watch does not work  :(
+            sass: {
+                files: ['<%= config.css_dest %>*.scss','<%= config.css_dest %>sass/*.scss','<%= config.css_dest %>css/*.css'],
+                tasks: ['sass:dist', 'cssmin']
+            }
+        }
+    });
+
+    // Load dependencies
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    // Run tasks
+    grunt.registerTask('default', ['sass']);
+
+};
